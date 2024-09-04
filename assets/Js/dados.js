0fb517562081c7f5bb5e3bbd4ff1b08b8e8e239a
Alex Farias Vaz dos Santos <alexfarias08032006@gmail.com>
2024-09-04 13:28:26 -0300

    3 e 4 dia de imersão

---
+++ assets/Js/dados.js
@@ -0,0 +1,30 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e força. Aos poucos anos, ela já coleciona títulos e é considerada uma das maiores ginastas da história do Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    },
+    {
+        titulo: "Neymar Jr.",
+        descricao: "Neymar Jr. é um dos maiores jogadores de futebol da atualidade e um dos brasileiros mais conhecidos no mundo.Com um talento inegável e uma habilidade única para driblar adversários, ele encantou milhões de fãs ao redor do globo. Desde muito jovem, Neymar demonstrou um potencial excepcional, chamando a atenção de grandes clubes europeus.Sua transferência para o Barcelona foi um marco em sua carreira, onde jogou ao lado de Lionel Messi e Luis Suárez, formando um dos trios de ataque mais temidos da história do futebol.",
+        link: "https://pt.wikipedia.org/wiki/Neymar"
+    },
+    {
+        titulo: "Flavia Saraiva",
+        descricao: "Flavia Saraiva é uma ginasta artística brasileira que conquistou o coração dos fãs com sua alegria e talento. Com apenas 24 anos, ela já coleciona diversas medalhas em competições nacionais e internacionais. ",
+        link: "https://pt.wikipedia.org/wiki/Flavia_Saraiva"
+    },
+    {
+        titulo: "Gabriel Medina",
+        descricao: "Gabriel Medina é um dos maiores nomes do surf brasileiro. Campeão mundial, ele é conhecido por suas manobras radicais e sua paixão pelo mar.",
+        link: "https://pt.wikipedia.org/wiki/Gabriel_Medina"
+    },
+
+];
+
+/* console.log(dados[0].descricao) */
